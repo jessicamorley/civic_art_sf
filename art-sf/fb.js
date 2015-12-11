1ff78373ecfa2bc12bf1304f99eb4bf16a7b3772
jkmorley <jmkmorley@gmail.com>
2015-12-11 22:34:28 +0000

    added new keys to fb.js

--- art-sf/fb.js
+++ art-sf/fb.js
@@ -1,7 +1,6 @@
 // facebook app settings
-//sorry i wont give out my fb user info because I don't want a scraper bot to find it on github
 module.exports = {
-	'appID' : 'APP_ID',
-	'appSecret' : 'SECRET_APP_ID',
+	'appID' : '1665356590409458',
+	'appSecret' : 'f5f554727753d4ee49366919dad76699',
 	'callbackUrl' : 'https://civic-art-sf-jkmorley.c9.io/auth/login/facebook/callback'
 }
